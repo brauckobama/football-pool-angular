8c0349b9d0a20492d8d3a61cf60d61f6f3c6585c
Matthew Brauckmann <MBrauckmann@anexinet.com>
2019-10-25 12:30:04 -0400

    Add new module for game detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSelectModule } from '@angular/material/select';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { GamesDetailComponent } from './games-detail/games-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GamesComponent,
-    PathNotFoundComponent
+    PathNotFoundComponent,
+    GamesDetailComponent
   ],
   imports: [
     BrowserModule,
